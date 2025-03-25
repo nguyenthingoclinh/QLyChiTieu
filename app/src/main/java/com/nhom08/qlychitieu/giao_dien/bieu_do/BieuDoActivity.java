d40ba90b0df34d7b27c5fcf42c3c75ab0c9cd886
ThanhNB_NBT <nguyenthanh120223@gmail.com>
2025-03-25 14:22:49 +0700

    Thêm giao diện

---
+++ app/src/main/java/com/nhom08/qlychitieu/giao_dien/bieu_do/BieuDoActivity.java
@@ -0,0 +1,26 @@
+package com.nhom08.qlychitieu.giao_dien.bieu_do;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.nhom08.qlychitieu.R;
+
+public class BieuDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.bieu_do);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
