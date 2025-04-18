14e6df9f00ff0150815404cea1b3c99bc04be9fb
ThanhNB_NBT <nguyenthanh120223@gmail.com>
2025-04-18 19:54:31 +0700

    Đã xong menu

---
+++ app/src/main/java/com/nhom08/qlychitieu/fragment/AddFragment.java
@@ -0,0 +1,21 @@
+package com.nhom08.qlychitieu.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nhom08.qlychitieu.R;
+
+public class AddFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add, container, false);
+    }
+}
