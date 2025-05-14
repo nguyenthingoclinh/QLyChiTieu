386f3b17d9a23569ae9653d59f7d3797da32ea1f
ThanhNB_NBT <nguyenthanh120223@gmail.com>
2025-05-14 20:09:54 +0700

    Giao diện ChartFragment

---
+++ app/src/main/java/com/nhom08/qlychitieu/LineChartFragment.java
@@ -0,0 +1,19 @@
+package com.nhom08.qlychitieu;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LineChartFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_line_chart, container, false);
+    }
+}
